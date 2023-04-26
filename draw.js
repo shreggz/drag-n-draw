38be1588b344f1510508f29eedf195c8b2645af9
Sridhar Bhagavathula <sridhar.bhagavathula@gmail.com>
2023-04-26 11:56:37 -0400

    Fix mouseover bug

--- draw.js
+++ draw.js
@@ -34,17 +34,10 @@ function createCanvas(gridSize) {
     let gridPixels = document.querySelectorAll(".pixel");
         gridPixels.forEach(gridPixel => {
             gridPixel.addEventListener('mouseover', colorPixels);
-            gridPixel.addEventListener('touchstart', colorPixels);
-            gridPixel.addEventListener('touchmove', colorPixels);
-            gridPixel.addEventListener('touchend', colorPixels);
         });    
 }
 
 function colorPixels(event) {
-    if (event.buttons === 1 || event.type === "touchstart") {
-        let touch = event.changedTouches ? event.changedTouches[0] : null;
-        let target = touch ? document.elementFromPoint(touch.clientX, touch.clientY) : this;
-
         switch (pixelColor) {
             case "rainbow":
                 this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
@@ -56,7 +49,7 @@ function colorPixels(event) {
                 this.style.backgroundColor = "#000000";
                 break;
         }
-    }
+    
 }
 
 function changePixelColor(event) {
